73483b40e4b250d8797cde621f039dc5ed8aa1aa
Josh Nussbaum <joshnuss@gmail.com>
2019-12-02 00:40:14 -0500

    Revert "Adds type=email for TextField"
    This reverts commit e005449c72e69ae3787255dc547e6ce0f0d943ae.

--- src/App.svelte
+++ src/App.svelte
@@ -67,7 +67,10 @@
 
   {#if !user}
   <Section title="Contact Information">
-    <InputField name="email" label="Email" type="email" bind:value={checkout.email} disabled={isSubmitting}/>
+    <label>
+      Email<br/>
+      <input bind:value={checkout.email} name="email" type="email" disabled={isSubmitting}/>
+    </label>
 
     <label>
       <input bind:checked={checkout.newsletter} name="newsletter" type="checkbox" disabled={isSubmitting}/>
@@ -77,7 +80,7 @@
   {/if}
 
   <Section title="Shipping Address">
-    {#if user && user.addresses.length > 0}
+    {#if user.addresses.length > 0}
       {#each user.addresses as address}
         <label>
           <input type=radio value={address.id} bind:group={checkout.shippingAddressId}/> {address.street}<br/> {address.municipality}, {address.region}<br/> {address.country}, {address.postalCode}
--- src/Components/InputField.svelte
+++ src/Components/InputField.svelte
@@ -1,12 +1,8 @@
 <script>
-  export let name, label, value, type="text", autocapitalize = false, disabled = false
+  export let name, label, value, autocapitalize = false, disabled = false
 </script>
 
 <label>
   {label}<br/>
-  {#if type == "email"}
-    <input bind:value={value} {name} type="email" {disabled} {autocapitalize}/>
-  {:else}
-    <input bind:value={value} {name} {disabled} {autocapitalize}/>
-  {/if}
+  <input bind:value={value} {name} {disabled} {autocapitalize}/>
 </label>
