548f1f79319f3999150052ca7bddb44c3e691a52
Josh Nussbaum <joshnuss@gmail.com>
2019-12-02 23:05:09 -0500

    Adds getShippingRates function

---
+++ src/Api/Checkout.js
@@ -0,0 +1,18 @@
+export function getShippingRates() {
+  return [
+    {
+      id: 1212,
+      carrier: "FedEx",
+      method: "Overnight",
+      amount: 59,
+      timeframe: "1-2 days"
+    },
+    {
+      id: 13939,
+      carrier: "USPS",
+      method: "SuperSaver",
+      amount: 5.99,
+      timeframe: "2-3 weeks"
+    }
+  ]
+}
